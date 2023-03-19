945b042f03fb5bfec728e1ba79819313b0f6d4ec
agustinsavoy1 <agustinsavoy1@gmail.com>
2023-03-18 23:25:55 -0300

    asd

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Form.css";
-
+import background from "../Assets/background.png";
 const Form = () => {
   const [pokemon, setPokemon] = useState([]);
   const [isActive, setIsActive] = useState(true);
@@ -9,14 +9,16 @@ const Form = () => {
   const id = Math.floor(Math.random() * 150) + 1;
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setPokemon(data);
-      });
+    // fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //     setPokemon(data);
+    //   });
+
+    get();
   }, []);
-  
+
   async function get() {
     await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
       .then((response) => response.json())
@@ -50,32 +52,42 @@ const Form = () => {
   };
 
   return (
-    <div
-      className="container-fluid pokemon-container"
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
-        margin: "0px",
-      }}
-    >
-      <div className="card pokemon-card" style={{ width: "18rem" }}>
-        <img
-          src={pokemon?.sprites?.front_default}
-          className={isActive ? "filter card-img-top" : "card-img-top"}
-          alt={pokemon.name}
-        />
-      </div>
-      <form onSubmit={adivinarPokemon}>
-        <input type="text" name="adivinar" autoComplete="off" />
-        <button type="submit">adivinar</button>
-        
-      </form>
+    <>
+      <div
+        className="container-fluid pokemon-container"
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+          margin: "0px",
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+          backgroundPosition: "center center",
+          backgroundSize: "cover",
+          paddingRight: "38rem",
+        }}
+      >
+        <div className="card pokemon-card">
+          <img
+            style={{
+              width: "20rem",
+            }}
+            src={pokemon?.sprites?.front_default}
+            className={isActive ? "filter card-img-top" : "card-img-top"}
+            alt={pokemon.name}
+          />
+        </div>
+        <form onSubmit={adivinarPokemon}>
+          <input type="text" name="adivinar" autoComplete="off" />
+          <button type="submit">adivinar</button>
+        </form>
 
-      <p>Your score:{score}</p>
-    </div>
+        <p>Your score:{score}</p>
+      </div>
+    </>
   );
 };
 
